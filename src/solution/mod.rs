6eca747315ac9c1b705a1494f0af4f36c4c05834
KarlvenK <karlven@outlook.com>
2022-04-22 15:10:41 +0800

    p396

--- src/solution/mod.rs
+++ src/solution/mod.rs
@@ -12,6 +12,7 @@ mod p0357_count_numbers_with_unique_digits;
 mod p0380_insert_delete_getrandom_01;
 mod p0386_lexicographical_numbers;
 mod p0388_longest_absolute_file_path;
+mod p0396_rotate_function;
 mod p0762_prime_number_of_set_bits_in_binary_representation;
 mod p0780_reaching_points;
 mod p0796_rotate_string;
---
+++ src/solution/p0396_rotate_function.rs
@@ -0,0 +1,37 @@
+use std::cmp::max;
+
+pub struct Solution {}
+
+impl Solution {
+    pub fn max_rotate_function(nums: Vec<i32>) -> i32 {
+        let sum: i32 = nums.iter().sum();
+        let mut pre = 0;
+        let mut ans = 0;
+        let n = nums.len();
+
+        for i in 0..n {
+            pre += nums[i] * (i as i32);
+        }
+        ans = pre;
+        for i in 1..n {
+            pre += sum - (n as i32) * nums[n - i];
+            ans = max(ans, pre);
+        }
+        ans
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_396() {
+        assert_eq!(
+            Solution::max_rotate_function(vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
+            330
+        );
+        assert_eq!(Solution::max_rotate_function(vec![4, 3, 2, 6]), 26);
+        assert_eq!(Solution::max_rotate_function(vec![100]), 0);
+    }
+}
