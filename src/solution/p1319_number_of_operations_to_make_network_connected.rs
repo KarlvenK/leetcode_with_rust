10e7c9b15972595f6685eab38be249db5ba9d523
KarlvenK <karlven@outlook.com>
2023-03-24 23:59:34 +0800

    p1319

---
+++ src/solution/p1319_number_of_operations_to_make_network_connected.rs
@@ -0,0 +1,40 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn make_connected(n: i32, connections: Vec<Vec<i32>>) -> i32 {
+        if n - 1 > connections.len() as i32 {
+            return -1;
+        }
+        let mut graph = Vec::new();
+        for i in 0..n as usize {
+            graph.push(Vec::new());
+        }
+        let mut vis = Vec::new();
+        for i in 0..n as usize {
+            vis.push(false);
+        }
+        for edge in connections {
+            let (u, v) = (edge[0] as usize, edge[1] as usize);
+            graph[u].push(v);
+            graph[v].push(u);
+        }
+
+        fn dfs(graph: &Vec<Vec<usize>>, vis: &mut Vec<bool>, node: usize) {
+            if vis[node] {
+                return;
+            }
+            vis[node] = true;
+            for v in &graph[node] {
+                dfs(graph, vis, *v);
+            }
+        }
+        let mut cnt = 0;
+        for i in 0..n as usize {
+            if !vis[i] {
+                dfs(&graph, &mut vis, i);
+                cnt += 1;
+            }
+        }
+        cnt - 1
+    }
+}
